67f3690a002fcdf8f44872943f56981f367cba15
Beksultan Kadyrbekov <kbeksss@gmail.com>
2019-11-17 21:13:07 +0600

    added ability to generate cards

--- src/App.js
+++ src/App.js
@@ -6,22 +6,20 @@ import CardDeck from "./CardDeck";
 
 class App extends Component{
     state = {
-        cards: ['jkjk'],
+        cards: []
     };
-    generate(){
-        // let deck = new CardDeck();
-        // let generatedCards = deck.getCards(5);
-        console.log(this.state);
-    }
-    render() {
+    generate = () => {
         let deck = new CardDeck();
         let cards = deck.getCards(5);
-        console.log(cards);
+        this.setState({cards});
+        console.log(this.state.cards);
+    };
+    render() {
         return(
             <div className="App">
                 <button onClick={this.generate}>Generate</button>
                 <div className='playingCards'>
-                    {cards.map((cardArr, index) => (
+                    {this.state.cards.map((cardArr, index) => (
                         <Card
                             key={index}
                             rank={cardArr[0].rank}
