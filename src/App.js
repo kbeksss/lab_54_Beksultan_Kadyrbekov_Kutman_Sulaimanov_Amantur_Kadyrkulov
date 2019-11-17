a8a48c3b91eeedc5543d951c2bef6dc28e549410
Beksultan Kadyrbekov <kbeksss@gmail.com>
2019-11-18 00:49:10 +0600

    added some conditions to check the cards type

--- src/App.js
+++ src/App.js
@@ -6,12 +6,54 @@ import CardDeck from "./CardDeck";
 
 class App extends Component{
     state = {
-        cards: []
+        cards: [],
+        type: ''
     };
     generate = () => {
         let deck = new CardDeck();
         let cards = deck.getCards(5);
         this.setState({cards});
+        let royalFlush = [];
+        let straightFlush = {
+            spades: 0,
+            diams: 0,
+            hearts: 0,
+            clubs: 0
+        };
+        setTimeout(() => {
+            for(let cardArr of this.state.cards){
+                if(cardArr[0].rank === 'a' || cardArr[0].rank ==='j' || cardArr[0].rank === 'q' || cardArr[0].rank === 'k'){
+                    royalFlush.push(cardArr[0].rank);
+                }
+                switch (cardArr[0].suit) {
+                    case 'spades':
+                        straightFlush.spades++;
+                        break;
+                    case 'diams':
+                        straightFlush.diams++;
+                        break;
+                    case 'hearts':
+                        straightFlush.hearts++;
+                        break;
+                    case 'clubs':
+                        straightFlush.clubs++;
+                        break;
+                    default:
+                        console.error('Incorrect suit value')
+                }
+            }
+            if(royalFlush.length === 5){
+                let type = 'Royal flush';
+                this.setState({type});
+                console.log('Royal flush', royalFlush);
+                royalFlush = [];
+            } else if(straightFlush.spades === 5 || straightFlush.diams === 5 || straightFlush.hearts === 5 || straightFlush.clubs === 5){
+                let type = 'Straight flush';
+                this.setState({type});
+                console.log('Straight flush', straightFlush);
+            }
+
+        }, 0);
     };
     render() {
         return(
@@ -25,6 +67,7 @@ class App extends Component{
                             suit={cardArr[0].suit}/>
                     ))}
                 </div>
+                <span className='type'>{this.state.type}</span>
             </div>
         )
     }
